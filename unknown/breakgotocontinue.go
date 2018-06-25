d0a52e21d29cb8372071f026c1c3cc370c77b2da
YoungNovice <2306915539@qq.com>
2018-06-25 23:34:22 +0800

    break goto continue

---
+++ unknown/breakgotocontinue.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	//testa()
+	//testb()
+	testc()
+}
+
+func testc()  {
+	for i := 0; i < 10; i++ {
+		for {
+			fmt.Println(i)
+			//continue LABEL
+			goto LABEL1
+		}
+	LABEL1:
+	}
+
+}
+
+func testb()  {
+	LABEL :
+		for i := 0; i < 10; i++ {
+			for {
+				fmt.Println(i)
+				continue LABEL
+			}
+		}
+}
+
+func testa() {
+	LABLE :
+		for   {
+			for i := 0; i < 10; i++ {
+				if i > 2 {
+					break LABLE
+				} else {
+					fmt.Println(i)
+				}
+			}
+		}
+
+}
