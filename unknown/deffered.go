59b864d294470810d7eef937a8653e1b3f21187b
YoungNovice <2306915539@qq.com>
2018-06-26 14:31:29 +0800

    defer go

---
+++ unknown/deffered.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+/**
+	执行结果为什么是这样的
+*/
+func main() {
+	var fs = [4]func(){}
+	for i:=0 ; i<4 ; i++ {
+		defer fmt.Println("defer i = ", i)
+
+		defer func() {
+			fmt.Println("defer_closure i = ", i)
+		}()
+
+		fs[i] = func(){
+			fmt.Println("closure i = ", i)
+		}
+	}
+
+	for _, f := range fs {
+		f()
+	}
+}
