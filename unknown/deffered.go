c7a596a713345a2741aee54cb320d479ef47e453
YoungNovice <2306915539@qq.com>
2018-06-26 14:35:58 +0800

    doc

--- unknown/deffered.go
+++ unknown/deffered.go
@@ -9,12 +9,12 @@ func main() {
 	var fs = [4]func(){}
 	for i:=0 ; i<4 ; i++ {
 		defer fmt.Println("defer i = ", i)
-
 		defer func() {
+			// 获取i的引用
 			fmt.Println("defer_closure i = ", i)
 		}()
-
-		fs[i] = func(){
+		fs[i] = func() {
+			// 获取i的引用
 			fmt.Println("closure i = ", i)
 		}
 	}
