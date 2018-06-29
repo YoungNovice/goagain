e03045dd503786b1073a968777f52b3f81b485b5
YoungNovice <2306915539@qq.com>
2018-06-29 09:46:32 +0800

    concurrent

---
+++ unknown/concurrentBase.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+// 通过通信共享内存 而不是通过共享内存来通信
+func main () {
+	// 配置一个chan
+	c := make(chan bool)
+	go func() {
+		// 睡眠三秒
+		time.Sleep(3 * time.Second)
+		fmt.Println("GO GO GO")
+		// 向通道中传递值
+		c <- true
+	}()
+	// 取出通道中的值
+	<- c	
+}
--- unknown/concurrentFirst.go
+++ unknown/concurrentFirst.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+// 主线程不会等待goroutine执行的 所以可能看不到打印的信息
+// 可以让主线程sleep 2秒来看到日志
 func main() {
 	go Go()
 	time.Sleep(2 * time.Second)
